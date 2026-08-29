c831ca347a1b7cffa0b4c9bbc1cd2fcc8e7a2592
Boxten <boxtenkeyes@gmail.com>
2026-08-28 21:46:31 -0400

    Create changelogs.lua

---
+++ data/internal/changelogs.lua
@@ -0,0 +1,165 @@
+return {
+	current = [[
+[NEW] • Added a "Show morph to other script users" toggle. (Main tab/Donor/Client sided morphs)
+[NEW] • Added a "OwOify script users" toggle. (Main tab/Donor/Actions)
+[NEW] • Added a "Blotspeak" toggle. (Main tab/Fun/Character)
+[NEW] • Added a "Clap" toggle. (Main tab/Fun/Emotes)
+
+[NEW] • Made it so the "Headsit player" input and toggle now uses Dandy's sitting animation.
+[NEW] • Made it so the "Machine aura" toggle doesn't force you on a machine if a Twisted is chasing you.
+[NEW] • Replaced the generic autofarming text for the Webhook logging to something more related to runs.
+
+[NEW] • Fixed the "Earn Investigator achievement" button not working at all.
+
+[NEW] • Attempted to fix the "Teleport to machine" button making proximity prompts disappear.
+
+[sV1286] • Re-designed the UI.
+[sV1286] • Fully re-factored and optimized the script.
+[sV1286] • Renamed a few elements and updated their descriptions.
+[sV1286] • Renamed the "Teleports" category to "Navigation".
+[sV1286] • Renamed the "Environment" category to "Visuals".
+[sV1286] • Removed the "Donor" section from (Main tab/Fun) and gave it its own category.
+[sV1286] • Gave the script changelogs its own category. (Settings tab)
+
+[sV1296] • Added a "Closed captions for Boxten" toggle. (Settings tab/UI Settings/Toons)
+[sV1296] • Added a "Closed captions for Poppy" toggle. (Settings tab/UI Settings/Toons)
+[sV1296] • Added a "Closed captions for Shrimpo" toggle. (Settings tab/UI Settings/Toons)
+[sV1295] • Added a "Elevator opens" option for the "Auto use ability condition" dropdown. (Main tab/Fun/Ability)
+[sV1294] • Added a "Smart auto heal" toggle. (Main tab/Automation/Ability)
+[sV1294] • Added a "Toggle chase theme" toggle. (Main tab/Fun/Game optimizations)
+[sV1294] • Added a "Chase theme volume" slider. (Main tab/Fun/Game optimizations)
+[sV1294] • Added a "Chase theme pitch" slider. (Main tab/Fun/Game optimizations)
+[sV1287] • Added a "Hide ESP billboards" toggle. (Main tab/Visuals/ESP Settings)
+[sV1287] • Added a "Auto arcade machine calibration" toggle. (Main tab/Automation/Player)
+[sV1286] • Added a "Adjust skillcheck position target" slider. (Main tab/Local Player/Utility)
+[sV1286] • Added a "Adjust skillcheck position" toggle. (Main tab/Local Player/Utility)
+[sV1286] • Added a "Adjust skillcheck size target" slider. (Main tab/Local Player/Utility)
+[sV1286] • Added a "Adjust skillcheck size" toggle. (Main tab/Local Player/Utility)
+[sV1286] • Added a "Show name tag" toggle. (Main tab/Visuals/Environment)
+[sV1286] • Added a "Black out name tag" input and toggle. (Main tab/Visuals/Environment)
+[sV1286] • Added a "Hide name tag" input and toggle. (Main tab/Visuals/Environment)
+[sV1286] • Added a "Card vote priority" dropdown. (Main tab/Automation/Player)
+[sV1286] • Added a "Quick mode" toggle. (Main tab/Automation/Autofarm settings)
+[sV1286] • Added a "Squirm mode" toggle. (Main tab/Automation/Autofarm settings)
+[sV1286] • Added a "Eggson mode" toggle. (Main tab/Automation/Autofarm settings)
+[sV1286] • Added a "Anti fail skill checks" toggle. (Main tab/Local Player/Utility)
+[sV1286] • Added a "Transparent elevator" toggle. (Main tab/Visuals/Utility)
+[sV1286] • Added a "Anti Ichor splotches" toggle. (Main tab/Local Player/Utility)
+[sV1286] • Added a "Auto prime machines" toggle. (Main tab/Automation/Player)
+[sV1286] • Added a "Kite distract" toggle. (Main tab/Automation/Distracting)
+[sV1286] • Added a "Kite distract island target toggle" toggle. (Main tab/Automation/Distracting)
+[sV1286] • Added a "Force stop extraction when teleporting to machine" toggle. (Settings tab/Script settings)
+[sV1286] • Added a "Auto teleport to machine condition" dropdown. (Main tab/Automation/Teleports)
+[sV1286] • Added a "Auto teleport to elevator condition" dropdown. (Main tab/Automation/Teleports)
+[sV1286] • Added a "Machine aura condition" Dropdown. (Main tab/Local Player/Utility)
+[sV1286] • Added a "Auto escape Twisted Squirm" toggle + input. (Main tab/Automation/Player)
+[sV1286] • Added a "Hide machine ESP conditions" dropdown. (Main tab/Visuals/ESP Settings)
+[sV1286] • Added a "Ichor puddle ESP" toggle. (Main tab/Visuals/ESPs)
+[sV1286] • Added a "Hover ESP" toggle. (Main tab/Visuals/ESP Settings)
+[sV1286] • Added a "Toggle barriers" toggle. (Main tab/Visuals/Utility)
+[sV1286] • Added a "Visuals" section. (Main tab/Fun)
+[sV1286] • Added a "Orbit player" toggle + input. (Main tab/Fun/Actions)
+[sV1286] • Added a "Adjust orbit radius" slider. (Main tab/Fun/Actions)
+[sV1286] • Added a "Adjust orbit speed" slider. (Main tab/Fun/Actions)
+[sV1286] • Added a "Item capacity limit" slider. (Main tab/Automation/Autofarm settings)
+[sV1286] • Added a "Achievements" section. (Main tab/Automation)
+[sV1286] • Added a "Autofarm statistics" section. (Main tab/Automation)
+[sV1286] • Added a "Toon ability replication" section. (Main tab/Local Player)
+[sV1286] • Added a "Access" section. (Main tab/Donor)
+[sV1286] • Added a "Actions" section. (Main tab/Donor)
+[sV1286] • Added a "Tools" section. (Main tab/Donor)
+[sV1286] • Added a "Teleports" section. (Main tab/Navigation)
+[sV1286] • Added a "Tweens" section. (Main tab/Navigation)
+[sV1286] • Added a "Pathfinding" section. (Main tab/Navigation)
+[sV1286] • Added a "Client sided morphs" section. (Main tab/Fun)
+[sV1286] • Added a "Client sided morphs" section. (Main tab/Donor)
+[sV1286] • Added a "Client sided Twisteds" section. (Main tab/Donor)
+[sV1286] • Added a "Client sided Items" section. (Main tab/Donor)
+
+[sV1286] • Removed the "Roleplay" section from (Main tab/Fun) due to all of its functions being patched.
+[sV1286] • Removed the "Twisteds" section from (Main tab/Local Player) due to most of the functions being patched.
+[sV1286] • Removed the "Buy event sticker" button due to it requiring event currency in order for it to work.
+[sV1286] • Removed the "Buy event skin" button due to it requiring event currency in order for it to work.
+
+[sV1296] • Made it so the "Fake Ichor" input and toggle also changes the Ichor label's text on the bottom left of the screen.
+[sV1296] • Adjusted the autofarm to make it so it doesn't make the player travel a ton to avoid your "Meters Walked" stat reaching gargantuan numbers.
+[sV1295] • Optimized some of the code a little due to the obfuscator creating loops that run in the background (?), resulting in the client crashing or disconnecting after a while.
+[sV1295] • Increased the maximum value of the "Auto boost player extraction speed minimum distance" slider from 200 to 600.
+[sV1295] • Increased the maximum value of the "Auto heal minimum distance" slider from 200 to 600.
+[sV1286] • Updated the autofarm.
+[sV1286] • Moved the Item, Buy, and Machine auras to (Main tab/Local Player/Utility).
+[sV1286] • Updated the list of available animations and made it so the animation target list only shows Toons that have animations that can only be applied on your current Toon.
+[sV1286] • Moved the "Animatons" section to the Main tab.
+[sV1286] • Moved the "Autofarm" section to the Main tab.
+[sV1286] • Made it so the "Encounter all Twisteds" button only encounters Twisteds that haven't spotted you yet.
+[sV1286] • Made it so the "Auto boost player extraction speed" toggle now targets appropriate players.
+[sV1286] • Made it so the encounter all Twisteds functions properly handles Twisted Blot.
+[sV1286] • Made it so the encounter all Twisteds functions teleports the player above the Twisteds instead of the front.
+[sV1286] • Made it so the Twisteds ESP shows Twisted Glisten's current sanity.
+
+[sV1295] • Fixed the script crashing on execution.
+[sV1293] • Fixed the checks for the donor gamepass.
+[sV1293] • Fixed the "Webhook logging" toggle not logging anything at all.
+[sV1291] • Fixed the "Pathfind to elevator" button only working when the elevator is open.
+[sV1291] • Fixed the functions in the "Pathfinding" section in Main tab/Navigation not making the player play their running / walk animations.
+[sV1288] • Fixed the donor gamepass checks not unlocking donor features.
+[sV1288] • Fixed a few typos in a few buttons.
+[sV1288] • Fixed the script kicking you when executing it in a roleplay server.
+[sV1288] • Fixed the "Lock toggle button" toggle making the toggle button inactive.
+
+[sV1297] • Attempted to fix the "Auto use items" toggle not using extraction / stealth items on some occasions.
+[sV1297] • Attempted to fix the "Unmorph Twisted" button not destroying the morph model.
+[sV1297] • Attempted to fix the "Unmorph" button not destroying the morph model.
+[sV1295] • Attempted to fix the "Smart auto heal" toggle not working properly.
+[sV1295] • Attempted to fix the "Auto boost player extraction speed" toggle not working properly.
+[sV1294] • Attempted to fix the "Machine aura" toggle not extracting the other side of a dual machine if the other side is occupied.
+[sV1294] • Attempted to fix the script crashing the client on higher floors.
+[sV1294] • Attempted to fix the autofarm not working properly.
+[sV1293] • Attempted to fix the functions in the "Client sided morphs" section in Main tab/Fun not working properly.
+[sV1291] • Attempted to fix configs not being able to save or load properly.
+[sV1291] • Attempted to fix certain UI elements overlapping with each other when switching categories.
+[sV1291] • Attempted to fix the "Hide ESP billboards" toggle not functioning properly.
+[sV1290] • Attempted to fix configs not being able to save or load properly.
+[sV1288] • Attempted to fix the script crashing or locking the camera in place in certain PC executors.
+[sV1288] • Attempted to fix the "Machine aura" toggle not working when descending to the next floor.
+[sV1288] • Attempted to fix the "Auto treadmill machine calibration" toggle not working properly on some occasions.
+[sV1286] • Attempted to fix the script not being able to load properly on some occasions.
+
+[sV1290] • Re-activated certain dialogue triggers for Boxten, Poppy, and Shrimpo.
+[sV1286] • Introduced Shrimpo.
+[sV1286] • Added more messages to Poppy.
+[sV1286] • Added an expand button to Poppy.
+[sV1286] • Added more messages to Boxten.
+[sV1286] • Fixed an issue where Boxten is unable to properly display or the time spent on a floor. ]],
+	
+	["1.2.9"] = [[
+• Added a "Exclude Yourself" toggle. (Main tab/Fun/Donor)
+• Added a "Ad Barrage Script Users" button. (Main tab/Fun/Donor)
+• Added a "Punch Script Uers Tool" toggle. (Main tab/Fun/Donor)
+• Added a "Uncoordinate Script Users" toggle. (Main tab/Fun/Donor)
+• Added a "Notify Script Users Message" input. (Main tab/Fun/Donor)
+• Added a "Notify Script Users" button. (Main tab/Fun/Donor)
+• Added a "Stack Script Users" button. (Main tab/Fun/Donor)
+
+• Brung back the "Semi-God Mode" toggle. (Main tab/Local Player/Behavior)
+
+• Included the amount of times you've been hit while autofarming for the Webhook death embed.
+• Improved the autofarm a little bit.
+
+• Fixed the "Buy Event Sticker" button not working at all.
+• Fixed the "Buy Event Skin" toggle not working at all.
+• Fixed the "Noclip" toggle still not working properly for some Toons.
+• Fixed the Donor functions not working at all.
+• Fixed the "Anti Ice" toggle not working on some occasions.
+
+• Attempted to fix the "Auto Heal Nearby Players" toggle not working for Ginger. ]],
+	
+	["1.2.8"] = [[
+• Added a "Exclude Blacklisted Items For "Pick Up All ..." Functions" toggle. (Settings tab/Settings/Script)
+
+• Improved the autofarm a little bit.
+• Updated the teleport bypass method.
+
+• Attempted to fix the Donor functions not working at all.
+• Attempted to fix the script not executing properly for Delta users. ]]
+}
