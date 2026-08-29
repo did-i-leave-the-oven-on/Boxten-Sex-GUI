0eeacb9da0e068a085c5394a9aee743da3674edb
Boxten <boxtenkeyes@gmail.com>
2026-08-28 21:46:08 -0400

    Create version info.lua

---
+++ data/internal/version info.lua
@@ -0,0 +1,27 @@
+return {
+	version = "1.3.0",
+	subversion = 1302,
+	lastupdated = "Friday, August 28, 2026",
+
+	sections = {
+		library = 22,
+		data = 22,
+		builder = 22,
+
+		mainsection = 22,
+		navigationsection = 22,
+		visualssection = 22,
+		localplayersection = 22,
+		automationsection = 22,
+		animationssection = 22,
+		funsection = 22,
+		donorsection = 22,
+
+		scriptsettingssection = 22,
+		uisettingssection = 22,
+		changelogssection = 22,
+
+		configssection = 22,
+		communityconfigssection = 22,
+	}
+}
